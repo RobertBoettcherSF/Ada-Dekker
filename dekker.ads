8bf96f5f75157542a9b6006ff1dfc15bf373db14
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-07-21 20:44:24 +0200

    Create dekker.ads

---
+++ dekker.ads
@@ -0,0 +1,7 @@
+--  dekker.ads
+--
+--  Specification for Dekker's Algorithm.
+--  Because we implement this as the main application procedure, the 
+--  specification only requires the subprogram declaration.
+
+procedure Dekker;
